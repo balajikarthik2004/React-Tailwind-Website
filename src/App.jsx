2b71d713f49268e30697f49d2fc16e85e1347c6f
balajikarthik004 <kbalajikbalaji879@gmail.com>
2024-04-12 21:28:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import About from './components/About'
+import Features from './components/Features'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Newletter from './components/Newsletter'
+import Pricing from './components/Pricing'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <Features/>
+    <About/>
+    <Pricing/>
+    <Newletter/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
